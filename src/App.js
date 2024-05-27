9f6422cc39b3888c40cf1213d699e27a29361fb6
vlrsrdev <vlrsrdev@gmail.com>
2024-05-27 12:30:27 +0000

    ver1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Main from "./components/Main";
+import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path='/' element={<Main />} />
+          <Route path='/weather' element={<Weather />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import bg from "/home/user/weathermap/weathermap/src/img/8562848_25501.jpg";
+
+export default function Main() {
+        const styles = {
+                backgroundImage: `url(${bg})`,
+                backgroundSize: 'cover',
+              };
+        return (
+                <div class="absolute top-0 left-0 -z-50 h-screen w-screen" style={styles}>
+                        <div class="flex justify-center items-center h-screen">
+                                <div class="bg-teal-500/75 p-4 rounded-lg shadow-md h-2/3 w-2/3">
+                                        <h1 class="font-mono text-4xl font-bold text-black">Welcome to WeatherMap</h1>
+                                        <br/>
+                                        <h1 class="font-mono text-2xl text-black">Weather app utilizing India's first crowd-supported weather infrastructure Weather Union!</h1>
+                                </div>
+                        </div>
+                </div>
+        )
+}
